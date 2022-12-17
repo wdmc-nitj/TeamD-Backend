84aa6be78b5086269ae89bc6c979a0814da5ddb0
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2022-12-17 12:54:01 +0530

    .

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const ugUpdateRoutes = require('./routes/ugUpdateRoutes');
 // Environment variables for database username and password
 const dbUser = process.env.atlasUser;
 const dbPass = process.env.atlasPassword;
-
 const dbName = 'admissions';
+const dbURI = `mongodb+srv://${dbUser}:${dbPass}@cluster0.5swxwob.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 // express app
 const app = express();
 
 // connect to MongoDB
-const dbURI = `mongodb+srv://${dbUser}:${dbPass}@cluster0.5swxwob.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 mongoose.connect(dbURI)
     .then((resutlt) => {
         app.listen(3000);
