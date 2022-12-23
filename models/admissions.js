76442f9ae0e7390e63980d121d68f6ad95b8b0a1
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2022-12-23 14:04:55 +0530

    added validators

--- models/admissions.js
+++ models/admissions.js
@@ -4,34 +4,81 @@ const Schema = mongoose.Schema;
 const updateSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        validator: {
+            validator: function (v) {
+                return v.length > 0;
+            },
+            message: props => `${props.value} is not a valid title!`
+        }
     },
     link: {
-        type: String
+        type: String,
+        // validates as a URL if is not empty
+        validator: {
+            validator: function (v) {
+                return v.length == 0 || /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }
     },
     visible: {
         type: Boolean,
-        default: true
+        default: true,
+        required: true,
+        // validates as a boolean
+        validator: {
+            validator: function (v) {
+                return typeof v === 'boolean';
+            },
+            message: props => `${props.value} is not a valid boolean!`
+        }
     },
     degree: {
         type: String,
         required: true,
-        enum: ['BTECH', 'MTECH-CCMT', 'MTECH-SELF', 'MSC', 'MBA', 'PHD', 'FOREIGN']
-    }
+        enum: ['BTECH', 'MTECH-CCMT', 'MTECH-SELF', 'MSC', 'MBA', 'PHD', 'FOREIGN'],
+        validator: {
+            validator: function (v) {
+                return v.length > 0;
+            },
+            message: props => `${props.value} is not a valid degree!`
+        }
+    },
 }, { timestamps: true });
 
 const linkSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        validator: {
+            validator: function (v) {
+                return v.length > 0;
+            },
+            message: props => `${props.value} is not a valid title!`
+        }
     },
     link: {
         type: String,
-        required: true
+        required: true,
+        validator: {
+            validator: function (v) {
+                return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(v);
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }
     },
     visible: {
         type: Boolean,
-        default: true
+        default: true,
+        required: true,
+        // validates as a boolean
+        validator: {
+            validator: function (v) {
+                return typeof v === 'boolean';
+            },
+            message: props => `${props.value} is not a valid boolean!`
+        }
     },
 }, { timestamps: true });
 
