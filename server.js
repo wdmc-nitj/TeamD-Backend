2552b63344300d06bb44998ab04dfb519b6cfdcf
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-01-03 11:37:30 +0530

    modified access control

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(morgan('dev'));
 // add access control headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next(); 
 });
