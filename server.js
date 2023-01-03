10618d8333d6abd42f4919f15d99f46f5efd0efd
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-01-03 11:27:45 +0530

    access control headers

--- server.js
+++ server.js
@@ -31,6 +31,14 @@ app.use(morgan('dev'));
 
 // routes
 
+// add access control headers
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next(); 
+});
+
 app.get('/health-check', (req, res) => res.send('OK'));
 
 app.use('/admissions', admissionsRoutes);
