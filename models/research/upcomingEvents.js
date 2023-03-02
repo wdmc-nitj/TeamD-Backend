eb86539f23f114e5ff5492b783afe2803c24649a
Kaushal Agrawal <kaushalagr.02@gmail.com>
2023-03-02 23:43:23 +0530

    schema_updated_ue

--- models/research/upcomingEvents.js
+++ models/research/upcomingEvents.js
@@ -2,13 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const upcomingEventSchema = new Schema({
-    title: {
+    title: {   // title of event
         type: String,
         required: true
     },
-    link: {
+    dateofEvent: {
+        type: String,
+        required: true,
+        validate: {
+            // dd-mm-yyyy
+            validator: function (v) {
+                const dateRegex = /^\d{1,2}-\d{1,2}-\d{4}$/;
+                return dateRegex.test(v);
+            },
+            message: props => `${props.value} is not a valid date! Please enter in dd-mm-yyyy format.`
+        }
+
+    },
+    venue: {   // venue of event
         type: String,
         required: true
+    },
+    organiser: {   // organiser of event
+        type: String,
+        required: true
+    },
+    timeofEvent: {   // time of event
+        type: String,
+        required: true
+    },
+    category: {     // category of Events
+        type: String,
+        required: true,
+        enum: ['conferences','seminars' ,'stc/fdp','workshops']
     },
     visible: {
         type: Boolean,
