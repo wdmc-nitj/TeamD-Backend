4da5bb0491cfb9a774141e4ad4b5524e9c5ecd27
lavish <goellavish10@gmail.com>
2023-03-29 14:46:11 +0530

    alumni routes

--- controllers/AlumniController.js
+++ controllers/AlumniController.js
@@ -39,6 +39,48 @@ const createAlumni = async (req, res) => {
     }
 };
 
+const deleteAlumni = async (req, res) => {
+    try {
+        const { alumniId } = req.params;
+
+        const alumni = await Alumni.findById(alumniId);
+
+        res.json(alumni);
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+};
+
+const searchAlumni = async (req, res) => {
+    try {
+        const { q, v } = req.query;
+        let alumni;
+        switch (q) {
+            case 'year':
+                alumni = await Alumni.find({
+                    passingYear: v,
+                });
+                break;
+            case 'name':
+                alumni = await Alumni.find({ name: { $regex: v } });
+                break;
+            case 'email':
+                alumni = await Alumni.find({ email: v });
+                break;
+            default:
+                break;
+        }
+
+        res.json(alumni);
+    } catch (err) {
+        console.log(err);
+        res.json(err);
+    }
+};
+
 module.exports = {
     createAlumni,
+    deleteAlumni,
+    searchAlumni,
 };
--- routes/alumniRoutes.js
+++ routes/alumniRoutes.js
@@ -2,8 +2,14 @@
 
 const express = require('express');
 const AlumniRouter = express.Router();
-const { createAlumni } = require('../controllers/AlumniController');
+const {
+    createAlumni,
+    deleteAlumni,
+    searchAlumni,
+} = require('../controllers/AlumniController');
 
 AlumniRouter.route('/create').post(createAlumni);
+AlumniRouter.route('/hide/:alumniId').post(deleteAlumni);
+AlumniRouter.route('/search').get(searchAlumni);
 
 module.exports = AlumniRouter;
